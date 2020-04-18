3208cd1631d329561f9f662d63a631cd7ec6d416
Briandepalmas <Student_GA110@Student-GA110.home>
2020-04-18 15:10:44 -0400

    added constructor and state, making api call with axios to a custom json to test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+export default class App extends Component {
+constructor(props){
+  super(props);
+    this.state={
+      data:[]
+    }
 }
 
-export default App;
+componentDidMount(){
+  let payload = {
+    token: "MclNmUrYSrRhFAB7tNMsyg",
+    data: {
+      name: "nameFirst",
+      email: "internetEmail",
+      phone: "phoneHome",
+      _repeat: 5
+    }
+  };
+  
+  axios({
+  method: "post",
+  url: "https://app.fakejson.com/q",
+  data: payload
+  }).then(function(resp) {
+  // Do something with fake data
+  });
+}
+
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
