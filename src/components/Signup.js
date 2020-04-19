22a39af3d04fbaedced3b160a0e062b70875dda4
Briandepalmas <Student_GA110@Student-GA110.home>
2020-04-19 15:50:04 -0400

    deleted name json key, change some variable names for clarity

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -3,27 +3,32 @@ import React, { Component } from 'react'
 export class Signup extends Component {
 //component state not being share as props
     state={
-        username:"",
+        username:'',
         password:''
     }
-    onSubmit = (e) => {
-        e.preventDefault();
-        this.props.Signup(this.state.username);
-        this.props.Signup(this.state.password)
-        this.setState({ username: '',password:'' });
+    onSubmit = (event) => {
+        event.preventDefault();
+        this.props.addUser(this.state.username);
+        this.props.addUser(this.state.password)
+        //resets username and password back to empty to receive new user input
+       // this.setState({ username: '',password:'' });
       }
 
       //targets name of input forms username and password
     onChange = (event) => this.setState({ [event.target.name]: event.target.value });
+    
     render() {
         return (
-            <form>
+            
+            <form onSubmit={this.onSubmit}>
                 Create UserName: 
                 <input type="text" name="username" placeholder="username" value={this.state.username}
                 onChange={this.onChange}/>
             
                 Create Password:
-                <input type="text" name="password" placeholder="enter password" value={this.state.password}></input>
+                <input type="text" name="password" placeholder="enter password" value={this.state.password}
+                onChange={this.onChange}/>
+                
                 <input type="submit" value="submit"></input>
             </form>
         )
